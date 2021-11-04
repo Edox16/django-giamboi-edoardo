1570eeaaf5cea77fa04789756b1a9798dcd97a4a
Edox16 <edoardogiamboi16@gmail.com>
2021-11-04 11:08:36 +0100

    push

--- prima_applicazione/apps.py
+++ prima_applicazione/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PrimaApplicazioneConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'prima_applicazione'
+
+class SecondaApplicazioneConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'seconda_applicazione'
