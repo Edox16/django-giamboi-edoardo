f8afc1c9f56b9a30f50ce6d8aebd4fe7c312203d
Edox16 <edoardogiamboi16@gmail.com>
2021-11-04 11:03:10 +0100

    Esegui

--- prima_applicazione/urls.py
+++ prima_applicazione/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin 
-from django.urls import path, include
+from django.urls import path 
+from prima_applicazione.views import homepage, welcome, lista, chi_siamo, index, variabili
 
-
+app_name="prima_applicazione"
 urlpatterns=[
-    path('admin/', admin.site.urls)
-    path('', include("prima_applicazione.urls", namespace="prima_applicazione")),
+    path('', homepage, name='homepage'),
+    path('welcome', welcome, name='welcome'),
+    path('lista', lista, name="lista"),
+    path('chi_siamo', chi_siamo, name="chi_siamo"),
+    path('variabili', variabili, name="variabili"),
+    path('index', index, name="index"),
 ]
